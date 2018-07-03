56e92ef793a1aec68c402d3ed144b7b955f6bdd5
Indu78 <Indu.abhilash@kefitech.com>
2018-07-03 17:59:26 +0530

    First commit

---
+++ src/test/java/mavenproject/smashxautomation/Home.java
@@ -0,0 +1,25 @@
+package mavenproject.smashxautomation;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Home {
+  @Test
+  public void f() {
+	  System.setProperty("webdriver.chrome.driver", "C:\\Users\\user\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.get("http://192.168.0.52:8080/");
+		driver.findElement(By.xpath("/html/body/form/div/div/div/div/input[1]")).sendKeys("Devi");
+		driver.findElement(By.xpath("/html/body/form/div/div/div/div/input[2]")).sendKeys("devi@1234");
+		driver.findElement(By.xpath("/html/body/form/div/div/div/div/button")).click();
+		driver.close();
+  }
+}
